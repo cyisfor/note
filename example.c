8e671fbefb5b4c1e9bf0f75a7db2346e905bffb4
autocommit <autocommit>
2019-06-14 03:48:28 +0000

    Add CMake support, and prettier source prefix cleaning

--- example.c
+++ example.c
@@ -3,7 +3,7 @@
 int main(int argc, char *argv[])
 {
     INFO("info level %d %s", argc, argv[0]);
-	INFO("this file is " __FILE__);
+	INFO("this file is " __FILE__ " but its prefix gets eliminated by comparing with the note.c __FILE__");
 	WARN("warning level");
 	ERROR("the program dies here");
 	ERROR("never get here");
