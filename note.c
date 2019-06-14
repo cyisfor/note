c5f24f2e33a45868ae6e82526686527901958ff4
autocommit <autocommit>
2019-06-14 03:43:50 +0000

    auto 0 40 379

--- note.c
+++ note.c
@@ -6,29 +6,29 @@
 static void note(const char* how, int hlen,
 								 const char* file, int flen,
 								 int line, const char* fmt, va_list arg) {
-	static ssize_t prefix_offset = -1;
-	static size_t maxflen = 0;
+	static size_t maxsourcelen = 0;
 	if(getenv("plain_log") == NULL) {
 		if(getenv("log_source") != NULL) {
-			if(prefix_offset == -1) {
-				int i = 0;
-				const char* myfile = __FILE__;
-				size_t mflen = sizeof(__FILE__)-1;
-				if(mflen > flen) {
-					mflen = flen;
-				}
-				for(i=0;i<mflen;++i) {					
-					if(file[i] != myfile[i]) break;
-				}
-				prefix_offset = i;
+			// eliminate common prefix between note/note.c and whatever source file it is
+			const char* myfile = __FILE__;
+			size_t mflen = sizeof(__FILE__)-1;
+			size_t i;
+			if(mflen > flen) {
+				mflen = flen;
 			}
-			fwrite(file+prefix_offset, flen-prefix_offset, 1, stderr);
-			fprintf(stderr,":%d",line);
-			if(maxflen < flen) {
-				maxflen = flen;
+			for(i=0;i<mflen;++i) {					
+				if(file[i] != myfile[i]) break;
+			}
+			if(i != flen) {
+				fwrite(file+i, flen-i, 1, stderr);
+			}
+			// the %d varies in size too, so it can change maxsourcelen even if flen is the same
+			size_t sourcelen = flen + fprintf(stderr,":%d",line);
+			if(maxsourcelen < sourcelen) {
+				maxsourcelen = sourcelen;
 			} else {
 				int i;
-				for(i=0;i<maxflen-flen;++i) {
+				for(i=0;i<maxsourcelen-sourcelen;++i) {
 					fputc(' ', stderr);
 				}
 			}
@@ -43,15 +43,15 @@ static void note(const char* how, int hlen,
 	fputc('\n',stderr);
 }
 
-#define BODY(name) \
-	va_list arg;																													\
-	va_start(arg, fmt);																										\
-	note(name,sizeof(name)-1,file,flen,line,fmt,arg);											\
+#define BODY(name)														\
+	va_list arg;														\
+	va_start(arg, fmt);													\
+	note(name,sizeof(name)-1,file,flen,line,fmt,arg);					\
 
 #define HEAD(ident) void ident ## f(const char* file, int flen, int line, const char* fmt, ...)
 
-#define DEFINE(ident,name) HEAD(ident) { \
-		BODY(name)																																\
+#define DEFINE(ident,name) HEAD(ident) {								\
+		BODY(name)														\
 }
 
 DEFINE(spam,"DEBUG");
